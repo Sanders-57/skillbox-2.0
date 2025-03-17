e11fe10ef92a3918e6aa67a5488c3350c52d094d
Sanders-57 <a.s.gusakov@yandex.ru>
2025-03-17 22:29:05 +0300

    Сделал первый урок

--- script/main.js
+++ script/main.js
@@ -1,1 +1,20 @@
-console.log('hello git');
+
+// let getArea = (x1,x2,y1,y2) => {
+//   let area = Math.abs((x2 - x1) * (y2 - y1))
+//   console.log(area);
+// }
+// getArea(-5, 10, 8, 5)
+
+
+// let a = 13.123456789,
+//     b = 2.123,
+//     n = 5
+// let procesion = Math.pow(10, n)
+// console.log(Math.round(a * procesion));
+
+let n = -3,
+    m = -10,
+    max = Math.max(n, m),
+    min = Math.min(n, m)
+
+console.log(Math.round(Math.random() * (max - min)) + min);
