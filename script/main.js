3f7396937701d0e1d9671c43e1140099b5b3baa0
Sanders-57 <a.s.gusakov@yandex.ru>
2025-03-19 22:16:19 +0300

    Сделал третий урок

--- script/main.js
+++ script/main.js
@@ -12,28 +12,81 @@
 // let procesion = Math.pow(10, n)
 // console.log(Math.round(a * procesion));
 
-let n = -3,
-    m = -10,
-    max = Math.max(n, m),
-    min = Math.min(n, m)
+// let n = -3,
+//     m = -10,
+//     max = Math.max(n, m),
+//     min = Math.min(n, m)
 
-console.log(Math.round(Math.random() * (max - min)) + min);
+// console.log(Math.round(Math.random() * (max - min)) + min);
 
 
-const password = '_-a'
+// const password = '_-a'
 
-if(password.length >= 4 && (password.includes('-') || password.includes('_'))){
-  console.log('Пароль надежный!');
-} else {
-  console.log('Пароль шляпа!');
-}
+// if(password.length >= 4 && (password.includes('-') || password.includes('_'))){
+//   console.log('Пароль надежный!');
+// } else {
+//   console.log('Пароль шляпа!');
+// }
 
-const name = 'Alex',
-      surName = 'Gusakov'
+// const name = 'Alex',
+//       surName = 'Gusakov'
 
-const name1 = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase()
+// const name1 = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase()
 
-const surName1 = surName.substring(0, 1).toUpperCase() + surName.substring(1).toLowerCase()
-console.log(name1, surName1);
+// const surName1 = surName.substring(0, 1).toUpperCase() + surName.substring(1).toLowerCase()
+// console.log(name1, surName1);
 
-name === name1 && surName === surName1 ? console.log('Имя осталось без изменений') : console.log('Имя было преобразовано');
+// name === name1 && surName === surName1 ? console.log('Имя осталось без изменений') : console.log('Имя было преобразовано');
+
+// const m = 100,
+//       n = 0,
+//       count = 100, 
+//       arr = []
+// let i
+
+
+// for (i = 0; i <= count; i++)  {
+// let elem = Math.round(Math.random() * (m - n)) + n
+// arr.push(elem)
+// }  
+
+// console.log(arr);
+
+
+// const str = 'Привет, мир!',
+//       arr = []
+
+// // console.log(str.length -1);
+// for(let i = str.length -1; i >= 0; i--){
+//   arr.push(str[i])
+// }
+// console.log(arr.join(''));
+
+// // const str1 = "→Привет, Мир!";
+// // let strRev = ""; // строка в обратном порядке
+// // for (i = str1.length-1; i >= 0; i--) {
+// //   strRev += str1[i];
+// // }
+// // console.log(strRev);
+
+// roadMines = [true, false, false, false, false, false, false, false, false, true] 
+// let i = 0
+
+// while (i <= roadMines.length){
+//   console.log(`Танк переместился на позицию ${i + 1}`);
+//   if(roadMines[i] === true){    
+//     console.log('Танк подбит');    
+//   }  
+//   i++
+//   if(roadMines[i] === true){    
+//     console.log('Танк убит');   
+//     break 
+//   }   
+// }
+
+const week = ['Понедельник', "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+let numbers  = Math.round(Math.random() * (31 - 1)) + 1
+console.log(numbers);
+console.log(numbers % 7);
+console.log(`Сегодня ${numbers} Января, день недели: ${week[(numbers % 7) - 1]}`);
